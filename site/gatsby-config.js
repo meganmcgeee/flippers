a50779e98fa815ff1e41a929c52352e48da13325
Nathan W <nathanwakefield1@gmail.com>
2019-08-15 15:30:58 -0700

    Add homedescriptionimage model for image next to description on homepage

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
         contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
           'Hometopimages',
           'Homedescriptions',
+          'Homedescriptionimages',
           'Lunchmenus',
           'Dinnermenus',
           'Menuphotobanners',
