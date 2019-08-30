70c4da6dbb8193780669d58499952611bc6bea40
Nathan W <nathanwakefield1@gmail.com>
2019-08-30 12:09:25 -0700

    remove non-existing component from about.js

--- site/src/pages/about.js
+++ site/src/pages/about.js
@@ -3,7 +3,6 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import Layout from "../components/layout"
-import SocialFeed from "../components/SocialFeed"
 import SEO from "../components/seo"
 
 const About = ({ data }) => (
