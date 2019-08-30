8e16af59952d5caac1556d845fe489defab9d5ce
Nathan W <nathanwakefield1@gmail.com>
2019-08-30 12:06:45 -0700

    deal with residual merge conflicts ??

--- site/src/pages/about.js
+++ site/src/pages/about.js
@@ -5,26 +5,17 @@ import { graphql } from 'gatsby'
 import Layout from "../components/layout"
 import SocialFeed from "../components/SocialFeed"
 import SEO from "../components/seo"
-import { Container, Row, Col  } from 'reactstrap';
-const About = () => (
+
+const About = ({ data }) => (
   <Layout>
     <SEO title="About" />
-    <Container>
-        <Row>
-          <Col>
-            <h1>Title here</h1>  
-            <p>lorem ipsum tasty pancake yyunlorem ipsum tasty pancake yyunlorem ipsum tasty pancake yyunlorem ipsum tasty pancake yyunlorem ipsum tasty pancake yyunlorem ipsum tasty pancake yyun</p>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col>
-            <h1>Title here</h1>  
-            <p>lorem ipsum tasty pancake yyunlorem ipsum tasty pancake yyunlorem ipsum tasty pancake yyunlorem ipsum tasty pancake yyunlorem ipsum tasty pancake yyunlorem ipsum tasty pancake yyun</p>
-          </Col>
-        </Row>
-      </Container>
-      <SocialFeed></SocialFeed>
+    <h1>About</h1>
+    <p>
+      {data.allStrapiAbouttexts.edges[0].node.textblock1}
+    </p>
+    <p>
+      {data.allStrapiAbouttexts.edges[0].node.textblock2}
+    </p>
   </Layout>
 )
 
